c423dfc3f6a7bb9e0b52c6757910f2458c3eb042
diogenescoello-droid <diogenes.coello@gestionderiesgos.gob.ec>
2026-08-10 17:44:16 -0500

    Agregar catálogo verificable de planes originales

---
+++ plan-sources.js
@@ -0,0 +1,12 @@
+window.SMART_RISK_PLAN_SOURCES={
+ generatedAt:'2026-08-10',
+ sourceFolder:'https://drive.google.com/drive/folders/1RtTv30phfaiv3InKxLxihFawKKN-wBAA',
+ plans:[
+  {province:'Guayas',territory:'Gobierno Provincial Del Guayas',documentName:'PLAN ACCION_PREFECTURA_ENOS_EPOCA LLUVIOSA Y SECA_2026_2027_firmado todos.pdf',url:'https://drive.google.com/file/d/1QnrVSNXHdz8uTSZ0VRAR-efC_b2qkYUB/view',status:'Firmado'},
+  {province:'Bolívar',territory:'Gobierno provincial de Bolivar',documentName:'1_0_PLAN_DE_ACCION_EL_NINO_2026__2027_VALIDADO_signed-signed-signed-1.pdf',url:'https://drive.google.com/file/d/1QmyQ4eLj-6gfxgMEEKgdQ4rWpd-lPlU-/view',status:'Validado y firmado'},
+  {province:'Los Ríos',territory:'Gobierno provincial de Los Rios',documentName:'PLAN_DE_ACCION_TERRITORIAL_PROVINCIAL_LOS_RIOS_ENOS_2026_2027_firmado.pdf',url:'https://drive.google.com/file/d/1OORaykZcWJHJe3zPWB9dXn_pCc84RKDT/view',status:'Firmado'},
+  {province:'Galápagos',territory:'Gobierno Provincial de Galápagos',documentName:'15.1. plan_de_accion_galapagos-signed-signed-signed.pdf',url:'https://drive.google.com/file/d/1M79_NXC7Q3oZsXTS8ZJnd5XTUaRfjtIp/view',status:'Firmado'},
+  {province:'Santa Elena',territory:'Gobierno Provincial Santa Elena',documentName:'GOBIERNO AUTÓNOMO DESCENTRALIZADO PROVINCIAL - ENOS - 2016 22 junio-signed-signed-signed-signed.pdf',url:'https://drive.google.com/file/d/1Pel8lEhcurc3yxLuUmAAwSp1QtRQ_yyA/view',status:'Firmado'},
+  {province:'Santa Elena',territory:'La Libertad',documentName:'Estructura_del_plan_de_accion_cantonal_LA LIBERTAD_fenomeno_del_niÑo.pdf',url:'https://drive.google.com/file/d/1PB2E0JxMh-XCHYlSOUuvs7CiUzPZ2W2B/view',status:'Documento disponible'}
+ ]
+};
