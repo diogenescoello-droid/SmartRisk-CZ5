d4222911da91762dd28d9090273a73f74947dd20
diogenescoello-droid <diogenes.coello@gestionderiesgos.gob.ec>
2026-08-23 13:39:17 -0500

    Expose current platform and F07 data dates

--- release-config.js
+++ release-config.js
@@ -6,7 +6,10 @@
     release: "V1.0.0 PILOTO ESTABLE",
     build: "1.0.0-piloto-estable",
     tag: "v1.0.0-piloto-estable",
-    dataCut: "2026-08-22",
+    dataCut: "2026-08-23",
+    platformUpdatedAt: "2026-08-23",
+    f07SyncedAt: "2026-08-23T17:57:45.664Z",
+    f07LatestSubmissionAt: "2026-08-21T17:39:13",
     channel: "production"
   });
 })();
